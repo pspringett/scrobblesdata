bd7b3b0f4e842e4dfd04985270ed42b8141ac780
Phill Springett <phillspringett@gmail.com>
2021-05-19 18:00:27 +0100

    First release of reading and writing old scrobbles.
    Good for 2015-2020, but shouldn't be used for 2020.

--- scrobbleswrite.py
+++ scrobbleswrite.py
@@ -15,94 +15,117 @@ tracks_path = os.path.join("h:\\", "dev", "scrobbles", "data", "tracks")
 # Track[1] is the artist
 # Track[2] is the album
 # Track[3] is the track
-# Tricksiness here is that we append to the the correct year file, so handle spltting over years.
-# Want to be clverer than this, and mroe important for the write tarcks.
-# for tracks to write:
-#   if fname is different
-#       if file is open, clsoe it
-#       open the new file
-#   write track
-
-
 def write_raw_tracks(tracks):
     def write_raw_tracks_to_file(fp, tracks):
         for fields in tracks:
+            # print(fields)
             fp.write(("\t".join(fields) + "\n"))
 
-    raw_years = {}
+    base_fname = "scrobbles.csv"
 
-    for track in tracks:
-        # print(track)
-        start_time = track[0]
-        dt = datetime.fromisoformat(start_time)
-        print(dt.year)
+    # create a nested dictionary, indexed by year, then month.
+    tracks_by_month = parse_tracks_to_write(tracks)
 
-        if dt.year not in raw_years:
-            raw_years[dt.year] = []
-        raw_years[dt.year].append(track)
+    # Walk the dictionary, writing out the files in the correct directories.
+    for year, months in tracks_by_month.items():
 
-    for year, tracks in raw_years.items():
+        for month, tracks in months.items():
 
-        fname = str(year) + "scrobbles.csv"
-        fqname = os.path.join(raw_tracks_path, fname)
+            # directory = "-".join([str(year), month])
+            fname = "-".join([str(year), month, base_fname])
+            fqpath = os.path.join(raw_tracks_path, str(year))
+            fqname = os.path.join(fqpath, fname)
+            # print("track file = {}".format(fqname))
+            Path(fqpath).mkdir(parents=True, exist_ok=True)
 
-        if os.path.exists(fqname):
-            print("Append raw tracks to filename={}".format(fname))
-            with open(fqname, "a") as fp:
-                write_raw_tracks_to_file(fp, tracks)
-
-        else:
-            print("write raw tracks to filename={}".format(fname))
-            with open(fqname, "w") as fp:
-                write_raw_tracks_to_file(fp, tracks)
-
+            if os.path.exists(fqname):
+                print("Append raw tracks to {}".format(fqname))
+                # print(tracks)
+                with open(fqname, "a", encoding="utf8") as fp:
+                    write_raw_tracks_to_file(fp, tracks)
+            else:
+                print("write raw tracks to {}".format(fqname))
+                with open(fqname, "w", encoding="utf8") as fp:
+                    write_raw_tracks_to_file(fp, tracks)
     return
 
 
 def write_tracks(tracks):
-    print("Write tracks")
+    def write_tracks_to_file(fp, tracks):
+        json.dump(tracks, fp, indent=4)
 
-    fname = "tracks.json"
+    base_fname = "tracks.json"
 
-    # create a nested dictionary, ndexed by year, then month.
-    tracks_by_year = {}
+    # create a nested dictionary, indexed by year, then month.
+    tracks_by_month = parse_tracks_to_write(tracks)
+
+    # Walk the dictionary, writing out the files in the correct directories.
+    for year, months in tracks_by_month.items():
+
+        for month, tracks in months.items():
+
+            fname = "-".join([str(year), month, base_fname])
+            fqpath = os.path.join(tracks_path, str(year))
+            fqname = os.path.join(fqpath, fname)
+            # print("track file = {}".format(fqname))
+            Path(fqpath).mkdir(parents=True, exist_ok=True)
+
+            if os.path.exists(fqname):
+                print("Append tracks to {}".format(fqname))
+                with open(fqname, "r") as fp:
+                    new_tracks = json.load(fp)
+                new_tracks.extend(tracks)
+                new_tracks.sort(key=lambda x: x[0], reverse=False)
+                with open(fqname, "w") as fp:
+                    json.dump(new_tracks, fp, indent=4)
+            else:
+                print("write tracks to {}".format(fqname))
+                with open(fqname, "w") as fp:
+                    json.dump(tracks, fp, indent=4)
+
+    return
+
+
+def parse_tracks_to_write(tracks):
+    tracks_by_month = {}
     for track in tracks:
         start_time = track[0]
         dt = datetime.fromisoformat(start_time)
         month = f"{dt:%m}"
-        print(dt.year)
-        print(month)
+        # print(dt.year)
+        # print(month)
 
-        if dt.year not in tracks_by_year:
-            tracks_by_year[dt.year] = {}
-            tracks_by_year[dt.year][month] = []
+        if dt.year not in tracks_by_month:
+            tracks_by_month[dt.year] = {}
+            tracks_by_month[dt.year][month] = []
 
-        if month not in tracks_by_year[dt.year]:
-            tracks_by_year[dt.year][month] = []
+        if month not in tracks_by_month[dt.year]:
+            tracks_by_month[dt.year][month] = []
 
-        tracks_by_year[dt.year][month].append(track)
+        tracks_by_month[dt.year][month].append(track)
+    return tracks_by_month
 
-    # Walk the dictionary, writing out the files in the correct directories.
-    for year, months in tracks_by_year.items():
+
+def write_tracks_by_month(tracks_by_month, tracks_path, fname, write_tracks_func):
+
+    for year, months in tracks_by_month.items():
 
         for month, tracks in months.items():
 
             directory = "-".join([str(year), month])
             fqpath = os.path.join(tracks_path, str(year), directory)
             fqname = os.path.join(fqpath, fname)
-            print("track file = {}".format(fqname))
+            print("write tracks to filename={}".format(fqname))
             Path(fqpath).mkdir(parents=True, exist_ok=True)
 
             if os.path.exists(fqname):
-                print("Append raw tracks to filename={}".format(fname))
+                print("Append tracks to filename={}".format(fqname))
                 with open(fqname, "a") as fp:
-                    json.dump(tracks, fp, indent=4)
+                    write_tracks_func(fp, tracks)
             else:
-                print("write raw tracks to filename={}".format(fname))
+                print("write tracks to filename={}".format(fqname))
                 with open(fqname, "w") as fp:
-                    json.dump(tracks, fp, indent=4)
-
-    return
+                    write_tracks_func(fp, tracks)
 
 
 def write_timestamp():
