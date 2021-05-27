ff35520ac834f031aa09d3fddccfb001bfeabdf4
Phill Springett <phillspringett@gmail.com>
2021-05-27 22:19:44 +0100

    Remov month strings (use global), and comment better.

--- fixup_old_scrobbles.py
+++ fixup_old_scrobbles.py
@@ -1,3 +1,11 @@
+"""
+Recreates mytracks from rawtracks.
+
+Converts from a tab-separated document which is raw data downloaded from last.fm, 
+to a json document, applying a bunch of filters and fixups.  
+
+It overwrites existing files.
+"""
 import os
 import sys
 import csv
@@ -6,27 +14,9 @@ import scrobblesfixup
 import json
 from optparse import OptionParser
 import scrobbleswrite
+import scrobblesglobal as glb
 
 import pprint
-
-# All of these shold end up under scrobbles\data eventually.
-fqpath = os.path.join("h:\\", "dev", "scrobbles", "data", "tracks")
-
-months_as_string = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-]
 
 
 def main():
@@ -46,15 +36,15 @@ def main():
     else:
         if options.month == 0:
             print("Fixup all scrobbles for one year")
-            for month in months_as_string:
-                tracks = read_tracks_month(fqpath, str(options.year), month)
+            for month in glb.months_as_string:
+                tracks = read_tracks_month(glb.tracks_path, str(options.year), month)
                 if tracks is not None:
                     new_tracks = fixup_tracks(fx, tracks)
                     scrobbleswrite.write_tracks(new_tracks, replace="yes")
 
         else:
             print("Fixup scrobbles for year and month")
-            tracks = read_tracks_month(fqpath, str(options.year), options.month2)
+            tracks = read_tracks_month(glb.tracks_path, str(options.year), options.month2)
             if tracks is not None:
                 new_tracks = fixup_tracks(fx, tracks)
                 scrobbleswrite.write_tracks(new_tracks, replace="yes")
